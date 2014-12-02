f277c3767476164c502fa52622449c703969ee75
bambanapt <bambanapt@hotmail.com>
2014-12-02 14:47:45 +0700

    edit laew eiei

--- lib/lovemac.rb
+++ lib/lovemac.rb
@@ -1,11 +1,11 @@
 def lovemac(number)
 	ans = ""
 	if (number%3).zero? && (number%5).zero?
-		ans = "Love"
-	elsif (number%3).zero?
-		ans = "Mac"
-	elsif (number%5).zero?
 		ans = "HateWindows"
+	elsif (number%3).zero?
+		ans = "Love"
+	elsif (number%5).zero?
+		ans = "Mac"
 	else 
 		ans = number	
 	end
