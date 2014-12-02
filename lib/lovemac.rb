632bde655b006d91102e37f66934abc8c92de72c
bambanapt <bambanapt@hotmail.com>
2014-12-02 14:45:32 +0700

    should be work!

--- lib/lovemac.rb
+++ lib/lovemac.rb
@@ -1,10 +1,10 @@
 def lovemac(number)
 	ans = ""
-	if (number%3).zero?
+	if (number%3).zero? && (number%5).zero?
 		ans = "Love"
-	elsif (number%5).zero?
+	elsif (number%3).zero?
 		ans = "Mac"
-	elsif (number%3).zero? && (number%5).zero?
+	elsif (number%5).zero?
 		ans = "HateWindows"
 	else 
 		ans = number	
