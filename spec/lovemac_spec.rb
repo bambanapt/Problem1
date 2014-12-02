11e1ce53d0c069ee7f2d4883b9deb30306e0ce3e
bambanapt <bambanapt@hotmail.com>
2014-12-02 13:44:34 +0700

    comment

--- spec/lovemac_spec.rb
+++ spec/lovemac_spec.rb
@@ -3,8 +3,8 @@ require "lovemac"
 
 describe Lovemac do 
 	describe "I can implement lovemac" do
-		(1..50).each do |number|
-			puts lovemac(number)
-		end
+		# (1..50).each do |number|
+		# 	puts lovemac(number)
+		# end
 	end
 end
